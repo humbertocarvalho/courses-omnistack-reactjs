2024a5ef65caf29c6021beb6174792600332dfb1
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-06-12 22:25:03 -0300

    Created Header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './Header.css';
 
 import logo from '../assets/logo.svg';
@@ -7,9 +9,13 @@ import camera from '../assets/camera.svg';
 export default function Header() {
   return (
     <header id='main-header'>
-      <div className='header-conten'>
-        <img src={logo} alt='InstaRocket' />
-        <img src={camera} alt='Enviar publicação' />
+      <div className='header-content'>
+        <Link to='/'>
+          <img src={logo} alt='InstaRocket' />
+        </Link>
+        <Link to='/new'>
+          <img src={camera} alt='Enviar publicação' />
+        </Link>
       </div>
     </header>
   );
